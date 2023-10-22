5ecf5790367607e76cd652642bd0d973dd4bb4a2
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-10-22 10:43:54 +0900

    style: Add opacity transition for command exam panel

--- src/components/molecules/CommandExamPanel.tsx
+++ src/components/molecules/CommandExamPanel.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useLayoutEffect, useState } from "react";
 import { CommandExam } from "../../composables/states/types";
 
 interface Props {
@@ -5,8 +6,18 @@ interface Props {
 }
 
 export const CommandExamPanel: React.FC<Props> = ({ commandExams }) => {
+  const [opacity, setOpacity] = useState(0);
+
+  useLayoutEffect(() => {
+    setOpacity(0);
+  }, [commandExams]);
+
+  useEffect(() => {
+    if (commandExams.length > 0) setOpacity(1);
+  }, [commandExams]);
+
   return (
-    <div>
+    <div className="transition-opacity duration-300" style={{ opacity }}>
       {commandExams.map((c) => (
         <div key={c.title} className="flex">
           {c.command ? <div className="mr-2">{c.command}:</div> : undefined}
