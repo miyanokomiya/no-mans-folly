52711daf12a6f96ca62113a0146f9c0ac10a1bd4
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-01-07 16:49:32 +0900

    fix: Make text range more precise along the arrow head

--- src/shapes/oneSidedArrow.ts
+++ src/shapes/oneSidedArrow.ts
@@ -36,31 +36,34 @@ export const struct: ShapeStruct<OneSidedArrowShape> = {
       width: arg.width ?? 100,
       height: arg.height ?? 50,
       textPadding: arg.textPadding ?? createBoxPadding([2, 2, 2, 2]),
-      headControl: arg.headControl ?? { x: 0.25, y: 0.75 },
+      headControl: arg.headControl ?? { x: 0.25, y: 0.5 },
       tailControl: arg.tailControl ?? { x: 1, y: 1 },
     };
   },
   getTextRangeRect(shape) {
-    const headDepth = (shape.height / 2) * (1 - shape.headControl.y);
+    const halfHeight = shape.height / 2;
+    const headDepth = halfHeight * (1 - shape.headControl.y);
     const headLength = shape.width * shape.headControl.x;
+    const bodyHeight = shape.height - headDepth * 2;
     const rect = {
       x: shape.p.x,
       y: shape.p.y + headDepth,
-      width: shape.width - headLength / 2,
-      height: shape.height - headDepth * 2,
+      width: shape.width - (bodyHeight / 2) * (headLength / halfHeight),
+      height: bodyHeight,
     };
     return shape.textPadding ? getPaddingRect(shape.textPadding, rect) : rect;
   },
 };
 
 function getPath(shape: OneSidedArrowShape): IVec2[] {
-  const headDepth = (shape.height / 2) * (1 - shape.headControl.y);
+  const halfHeight = shape.height / 2;
+  const headDepth = halfHeight * (1 - shape.headControl.y);
   const headLength = shape.width * shape.headControl.x;
   return [
     { x: shape.p.x, y: shape.p.y + headDepth },
     { x: shape.p.x + shape.width - headLength, y: shape.p.y + headDepth },
     { x: shape.p.x + shape.width - headLength, y: shape.p.y },
-    { x: shape.p.x + shape.width, y: shape.p.y + shape.height / 2 },
+    { x: shape.p.x + shape.width, y: shape.p.y + halfHeight },
     { x: shape.p.x + shape.width - headLength, y: shape.p.y + shape.height },
     { x: shape.p.x + shape.width - headLength, y: shape.p.y + shape.height - headDepth },
     { x: shape.p.x, y: shape.p.y + shape.height - headDepth },
