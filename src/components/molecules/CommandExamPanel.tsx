d4ffc7d197632ad2faa11721c49fcb7d177e7d30
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-02-10 12:56:13 +0900

    style: Polish style

--- src/components/molecules/CommandExamPanel.tsx
+++ src/components/molecules/CommandExamPanel.tsx
@@ -17,7 +17,7 @@ export const CommandExamPanel: React.FC<Props> = ({ commandExams }) => {
   }, [commandExams]);
 
   return (
-    <div className="transition-opacity duration-300" style={{ opacity }}>
+    <div className="transition-opacity duration-500 bg-gray-300 bg-opacity-80 px-2 rounded" style={{ opacity }}>
       {commandExams.map((c) => (
         <div key={c.title} className="flex">
           {c.command ? <div className="mr-2">{c.command}:</div> : undefined}
