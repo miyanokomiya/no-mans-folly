53c840d7dcb1e3c59343163f76ec0fbad2d48871
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-03-09 22:58:16 +0900

    feat: Zoom in/out via holding zoom buttons
    - It's more convenient for touch devices

--- src/components/AppFootbar.tsx
+++ src/components/AppFootbar.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useState } from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { AppStateContext, AppStateMachineContext } from "../contexts/AppContext";
 import { PopupButton } from "./atoms/PopupButton";
 import { getWrapperRect } from "../utils/geometry";
@@ -10,23 +10,23 @@ import iconHelp from "../assets/icons/help.svg";
 
 export const AppFootbar: React.FC = () => {
   const sm = useContext(AppStateMachineContext);
-  const smctx = useContext(AppStateContext);
+  const { setZoom, getScale, getShapeComposite, setViewport } = useContext(AppStateContext);
   const [popupedKey, setPopupedKey] = useState("");
 
-  const scale = Math.round((1 / smctx.getScale()) * 100);
-  const handleScaleDown = useCallback(() => {
-    smctx.zoomView(1, true);
-  }, [smctx]);
-  const handleScaleUp = useCallback(() => {
-    smctx.zoomView(-1, true);
-  }, [smctx]);
+  const scale = Math.round((1 / getScale()) * 100);
+  const handleZoomOut = useCallback(() => {
+    setZoom(getScale() * 1.02, true);
+  }, [setZoom, getScale]);
+  const handleZoomIn = useCallback(() => {
+    setZoom(getScale() / 1.02, true);
+  }, [setZoom, getScale]);
   const handleScaleFit = useCallback(() => {
-    const shapeComposite = smctx.getShapeComposite();
-    smctx.setViewport(getWrapperRect(shapeComposite.shapes.map((s) => shapeComposite.getWrapperRect(s))), 80);
-  }, [smctx]);
+    const shapeComposite = getShapeComposite();
+    setViewport(getWrapperRect(shapeComposite.shapes.map((s) => shapeComposite.getWrapperRect(s))), 80);
+  }, [getShapeComposite, setViewport]);
   const handleScale100 = useCallback(() => {
-    smctx.setZoom(1, true);
-  }, [smctx]);
+    setZoom(1, true);
+  }, [setZoom]);
 
   const onClickPopupButton = useCallback((value: string) => {
     setPopupedKey((key) => (key === value ? "" : value));
@@ -53,11 +53,9 @@ export const AppFootbar: React.FC = () => {
   }, [sm]);
 
   return (
-    <div className="p-1 border rounded bg-white flex gap-1 items-center">
-      <div className="flex">
-        <button type="button" className="w-4 h-8 rounded" onClick={handleScaleDown}>
-          -
-        </button>
+    <div className="p-1 border rounded bg-white flex gap-2 items-center">
+      <div className="flex gap-1 items-center">
+        <ZoomButton onZoom={handleZoomOut}>-</ZoomButton>
         <OutsideObserver onClick={handleCloseScalePopup}>
           <PopupButton
             name="scale"
@@ -73,16 +71,16 @@ export const AppFootbar: React.FC = () => {
             <div className="flex items-center justify-center w-12">{scale}%</div>
           </PopupButton>
         </OutsideObserver>
-        <button type="button" className="w-4 h-8 rounded" onClick={handleScaleUp}>
-          +
+        <ZoomButton onZoom={handleZoomIn}>+</ZoomButton>
+      </div>
+      <div className="flex gap-1 items-center">
+        <button type="button" className="w-8 h-8 border p-1 rounded" onClick={onUndo}>
+          <img src={iconUndo} alt="Undo" />
+        </button>
+        <button type="button" className="w-8 h-8 border p-1 rounded" onClick={onRedo}>
+          <img src={iconRedo} alt="Redo" />
         </button>
       </div>
-      <button type="button" className="w-8 h-8 border p-1 rounded" onClick={onUndo}>
-        <img src={iconUndo} alt="Undo" />
-      </button>
-      <button type="button" className="w-8 h-8 border p-1 rounded" onClick={onRedo}>
-        <img src={iconRedo} alt="Redo" />
-      </button>
       {process.env.CONTACT_FORM_URL ? (
         <OutsideObserver onClick={handleCloseHelpPopup}>
           <PopupButton
@@ -105,5 +103,55 @@ export const AppFootbar: React.FC = () => {
         </OutsideObserver>
       ) : undefined}
     </div>
+  );
+};
+
+interface ZoomButtonProps {
+  children: React.ReactNode;
+  onZoom?: () => void;
+}
+
+const ZoomButton: React.FC<ZoomButtonProps> = ({ children, onZoom }) => {
+  const [down, setDown] = useState(false);
+
+  const handleDown = useCallback((e: React.PointerEvent) => {
+    e.preventDefault();
+    setDown(true);
+  }, []);
+
+  const handleUp = useCallback(() => {
+    setDown(false);
+  }, []);
+
+  const handleContextMenu = useCallback((e: React.MouseEvent) => {
+    // For long touch
+    e.preventDefault();
+  }, []);
+
+  const animRef = useRef(0);
+  const handleZoomRef = useRef((_delta?: number) => {});
+  handleZoomRef.current = (timestamp = 0) => {
+    if (timestamp) {
+      onZoom?.();
+    }
+    animRef.current = requestAnimationFrame(handleZoomRef.current);
+  };
+  useEffect(() => {
+    if (!down) return;
+
+    handleZoomRef.current();
+    return () => cancelAnimationFrame(animRef.current);
+  }, [down]);
+
+  return (
+    <button
+      type="button"
+      className="w-8 h-8 rounded border select-none touch-none text-xl flex items-center justify-center"
+      onPointerDown={handleDown}
+      onPointerUp={handleUp}
+      onContextMenu={handleContextMenu}
+    >
+      {children}
+    </button>
   );
 };
