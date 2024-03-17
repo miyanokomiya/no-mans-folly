06722103ffb264ac885a0101583b74676a5e29b9
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-03-17 15:46:26 +0900

    fix: Set dev api host by default

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     "process.env.APP_VERSION": JSON.stringify(process.env.npm_package_version),
     "process.env.CONTACT_FORM_URL": JSON.stringify(process.env.CONTACT_FORM_URL),
     "process.env.ASSETS_PATH": JSON.stringify(process.env.ASSETS_PATH ?? "https://assets.no-mans-folly.com/"),
-    "process.env.API_HOST": JSON.stringify(process.env.API_HOST ?? "https://workers.no-mans-folly.com/"),
+    "process.env.API_HOST": JSON.stringify(process.env.API_HOST ?? "http://localhost:8787/"),
     // Dev project: https://console.cloud.google.com/apis/dashboard?project=test-no-mans-folly
     "process.env.GOOGLE_CLIENT_ID": JSON.stringify(
       process.env.GOOGLE_CLIENT_ID ?? "523071152184-qoep1r9m6erdtr75tc31faorivohrpok.apps.googleusercontent.com",
