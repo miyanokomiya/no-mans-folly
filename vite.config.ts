2eec4135cc8a2baee9356fd24de7af32f028e566
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-03-17 12:54:02 +0900

    fix: Replace Google API env vars to ones of test project
    - These values aren't secret

--- vite.config.ts
+++ vite.config.ts
@@ -23,11 +23,12 @@ export default defineConfig({
     "process.env.CONTACT_FORM_URL": JSON.stringify(process.env.CONTACT_FORM_URL),
     "process.env.ASSETS_PATH": JSON.stringify(process.env.ASSETS_PATH ?? "https://assets.no-mans-folly.com/"),
     "process.env.API_HOST": JSON.stringify(process.env.API_HOST ?? "http://localhost:8787/"),
+    // Dev project: https://console.cloud.google.com/apis/dashboard?project=test-no-mans-folly
     "process.env.GOOGLE_CLIENT_ID": JSON.stringify(
-      process.env.GOOGLE_CLIENT_ID ?? "443651764160-4klkt2fdo9eahdhoeg2ia5tsnsj5tk13.apps.googleusercontent.com",
+      process.env.GOOGLE_CLIENT_ID ?? "523071152184-qoep1r9m6erdtr75tc31faorivohrpok.apps.googleusercontent.com",
     ),
     "process.env.GOOGLE_API_KEY": JSON.stringify(
-      process.env.GOOGLE_API_KEY ?? "AIzaSyDen1FgZbOBII7KYbYITNm7GZOsS1Ax-NM",
+      process.env.GOOGLE_API_KEY ?? "AIzaSyAAl4rKH1gNhAFcmW_KUqw2oGcgZgfAi34",
     ),
   },
 });
