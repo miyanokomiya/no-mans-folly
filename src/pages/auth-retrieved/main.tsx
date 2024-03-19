1046e5e10aa411039330860d5419869ed826cf5d
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-03-19 21:24:34 +0900

    fix: Polish oauth callback page

--- src/pages/auth-retrieved/main.tsx
+++ src/pages/auth-retrieved/main.tsx
@@ -5,9 +5,10 @@ import "../../index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <div className="w-screen h-screen flex flex-col items-center justify-center gap-4 text-center text-lg">
-      <div>
-        <p>Auth to the workspace is retrieved.</p>
-        <p>Data syncing will work on next update in the original page.</p>
+      <h1 className="text-2xl font-semibold">Authentication successful</h1>
+      <div className="mt-2">
+        <p>You can open external workspace</p>
+        <p>and data syncing will work on next update in the original page.</p>
       </div>
       <button
         type="button"
@@ -17,7 +18,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         Close this page
       </button>
       <div>
-        <p>Above button may not work depending on the situation.</p>
+        <p>Above button may not work depending on environment.</p>
         <p>Please, manually close this page in such case.</p>
       </div>
     </div>
