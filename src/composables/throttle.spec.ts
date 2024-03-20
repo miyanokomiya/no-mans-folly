bbc8a3629ca002c3cdf1c79e365f243101cdebe2
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-03-20 21:29:54 +0900

    feat: Create leveled throttle

--- src/composables/throttle.spec.ts
+++ src/composables/throttle.spec.ts
@@ -1,5 +1,5 @@
-import { expect, describe, test, vi } from "vitest";
-import { newThrottle } from "./throttle";
+import { expect, describe, test, vi, afterEach, beforeEach } from "vitest";
+import { newLeveledThrottle, newThrottle } from "./throttle";
 
 async function sleep(time: number) {
   return new Promise((resolve) => {
@@ -111,6 +111,137 @@ describe("newThrottle", () => {
 
       expect(mock).toHaveBeenCalledTimes(1);
       expect(mock).toHaveBeenNthCalledWith(1, 20);
+    });
+  });
+});
+
+describe("newLeveledThrottle", () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  test("should omit internal calling", () => {
+    const fn = vi.fn();
+    const t = newLeveledThrottle(fn, [10]);
+    t();
+    t();
+    t();
+    vi.advanceTimersByTime(20);
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+  test("should recall after waiting the interval", () => {
+    const fn = vi.fn();
+    const t = newLeveledThrottle(fn, [10]);
+    t();
+    expect(fn).toHaveBeenCalledTimes(0);
+    vi.advanceTimersByTime(20);
+    expect(fn).toHaveBeenCalledTimes(1);
+    t();
+    expect(fn).toHaveBeenCalledTimes(1);
+    vi.advanceTimersByTime(20);
+    expect(fn).toHaveBeenCalledTimes(2);
+  });
+  test("should pass the latest args", () => {
+    const mock = vi.fn();
+    const fn = (val1: number, val2: number) => mock(val1, val2);
+    const t = newLeveledThrottle(fn, [10]);
+    t(10, 100);
+    vi.advanceTimersByTime(5);
+    t(20, 200);
+    vi.advanceTimersByTime(6);
+    expect(mock).toHaveBeenCalledTimes(1);
+    expect(mock).toHaveBeenCalledWith(20, 200);
+    t(30, 300);
+    vi.advanceTimersByTime(10);
+    expect(mock).toHaveBeenCalledTimes(2);
+    expect(mock).toHaveBeenCalledWith(30, 300);
+  });
+
+  test("should level up inverval each time the throttle is called in time", () => {
+    const fn = vi.fn();
+    const t = newLeveledThrottle(fn, [10, 20, 30]);
+    t();
+    expect(fn).toHaveBeenCalledTimes(0);
+    expect(t.getLevel()).toBe(0);
+    vi.advanceTimersByTime(11);
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(t.getLevel()).toBe(1);
+    t();
+    vi.advanceTimersByTime(11);
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(t.getLevel()).toBe(1);
+    vi.advanceTimersByTime(11);
+    expect(fn).toHaveBeenCalledTimes(2);
+    expect(t.getLevel()).toBe(2);
+    t();
+    vi.advanceTimersByTime(11);
+    expect(fn).toHaveBeenCalledTimes(2);
+    expect(t.getLevel()).toBe(2);
+    vi.advanceTimersByTime(11);
+    expect(fn).toHaveBeenCalledTimes(2);
+    expect(t.getLevel()).toBe(2);
+    vi.advanceTimersByTime(11);
+    expect(fn).toHaveBeenCalledTimes(3);
+    expect(t.getLevel()).toBe(2);
+  });
+
+  test("should level down inverval each time the throttle isn't called in time", () => {
+    const fn = vi.fn();
+    const t = newLeveledThrottle(fn, [10, 20, 30]);
+    t();
+    vi.advanceTimersByTime(11);
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(t.getLevel()).toBe(1);
+    t();
+    vi.advanceTimersByTime(22);
+    expect(fn).toHaveBeenCalledTimes(2);
+    expect(t.getLevel()).toBe(2);
+    vi.advanceTimersByTime(22);
+    expect(t.getLevel()).toBe(2);
+    vi.advanceTimersByTime(11);
+    expect(t.getLevel()).toBe(1);
+    t();
+    vi.advanceTimersByTime(22);
+    expect(fn).toHaveBeenCalledTimes(3);
+    expect(t.getLevel()).toBe(2);
+    vi.advanceTimersByTime(33);
+    expect(t.getLevel()).toBe(1);
+    vi.advanceTimersByTime(22);
+    expect(t.getLevel()).toBe(0);
+  });
+
+  describe("flush", () => {
+    test("should flush current throttled item", () => {
+      const mock = vi.fn();
+      const fn = (val1: number) => mock(val1);
+      const t = newLeveledThrottle(fn, [10]);
+      t(10);
+      t.flush();
+      expect(mock).toHaveBeenCalledTimes(1);
+      expect(mock).toHaveBeenNthCalledWith(1, 10);
+      vi.advanceTimersByTime(15);
+      expect(mock).toHaveBeenCalledTimes(1);
+      expect(t.getLevel()).toBe(0);
+    });
+  });
+
+  describe("clear", () => {
+    test("should clear current throttled item", () => {
+      const mock = vi.fn();
+      const fn = (val1: number) => mock(val1);
+      const t = newLeveledThrottle(fn, [10]);
+      t(10);
+      t.clear();
+      vi.advanceTimersByTime(15);
+      t(20);
+      vi.advanceTimersByTime(15);
+
+      expect(mock).toHaveBeenCalledTimes(1);
+      expect(mock).toHaveBeenNthCalledWith(1, 20);
+      expect(t.getLevel()).toBe(0);
     });
   });
 });
--- src/composables/throttle.ts
+++ src/composables/throttle.ts
@@ -81,3 +81,114 @@ export function newThrottle<T extends (...args: any[]) => void>(fn: T, interval:
 
   return throttle;
 }
+
+export function newLeveledThrottle<T extends (...args: any[]) => void>(fn: T, intervals: number[]) {
+  let wait: undefined | "wait" | "cooldown";
+  let currentArgs: Parameters<T>;
+  let timer = 0;
+  let cooldowntimer = 0;
+  let level = 0;
+
+  function getInterval() {
+    return intervals[Math.min(level, intervals.length - 1)];
+  }
+
+  function throttle(...args: Parameters<T>) {
+    currentArgs = args;
+    callback.dispatch(true);
+    if (wait) {
+      wait = "wait";
+      return;
+    }
+
+    wait = "wait";
+    tick();
+  }
+
+  function tick() {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    if (cooldowntimer) {
+      clearTimeout(cooldowntimer);
+    }
+
+    timer = setTimeout(() => {
+      timer = 0;
+      if (wait !== "wait") {
+        wait = undefined;
+        return;
+      }
+
+      fn(...currentArgs);
+      wait = "cooldown";
+      level = Math.min(level + 1, intervals.length - 1);
+      callback.dispatch(false);
+      tick();
+      cooldownTick();
+    }, getInterval()) as any;
+  }
+
+  function cooldownTick() {
+    if (cooldowntimer) {
+      clearTimeout(cooldowntimer);
+    }
+
+    cooldowntimer = setTimeout(() => {
+      cooldowntimer = 0;
+      if (wait === "wait") return;
+
+      level = Math.max(level - 1, 0);
+      cooldownTick();
+    }, getInterval()) as any;
+  }
+
+  throttle.flush = function () {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    if (cooldowntimer) {
+      clearTimeout(cooldowntimer);
+    }
+    timer = 0;
+    cooldowntimer = 0;
+    level = 0;
+
+    if (wait === "wait") {
+      fn(...currentArgs);
+      callback.dispatch(false);
+    }
+    wait = undefined;
+  };
+
+  throttle.clear = function (): boolean {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    if (cooldowntimer) {
+      clearTimeout(cooldowntimer);
+    }
+    timer = 0;
+    cooldowntimer = 0;
+    level = 0;
+
+    if (wait === "wait") {
+      wait = undefined;
+      callback.dispatch(false);
+      return true;
+    }
+
+    wait = undefined;
+    return false;
+  };
+
+  const callback = newCallback<boolean>();
+
+  throttle.watch = function (fn: (pending: boolean) => void): () => void {
+    return callback.bind(fn);
+  };
+
+  throttle.getLevel = () => level;
+
+  return throttle;
+}
