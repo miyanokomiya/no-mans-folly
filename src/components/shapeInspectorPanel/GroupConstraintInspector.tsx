6eb23b40a8232c33eaae0ecdf178f59461339538
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-03-29 08:21:17 +0900

    feat: Use icons for group constraints UI

--- src/components/shapeInspectorPanel/GroupConstraintInspector.tsx
+++ src/components/shapeInspectorPanel/GroupConstraintInspector.tsx
@@ -1,28 +1,17 @@
 import { useCallback } from "react";
 import { GroupConstraint, Shape } from "../../models";
-import { SelectInput } from "../atoms/inputs/SelectInput";
-import { InlineField } from "../atoms/InlineField";
-import { BlockGroupField } from "../atoms/BlockField";
+import { BlockField, BlockGroupField } from "../atoms/BlockField";
+import gcIcon0 from "../../assets/icons/group_constraints_0.svg";
+import gcIcon1 from "../../assets/icons/group_constraints_1.svg";
+import gcIcon2 from "../../assets/icons/group_constraints_2.svg";
+import gcIcon3 from "../../assets/icons/group_constraints_3.svg";
+import gcIcon4 from "../../assets/icons/group_constraints_4.svg";
+import gcIcon5 from "../../assets/icons/group_constraints_5.svg";
+import gcIcon6 from "../../assets/icons/group_constraints_6.svg";
 
-const gcVOptions = [
-  { value: "0", label: "none" },
-  { value: "1", label: "top" },
-  { value: "2", label: "height" },
-  { value: "3", label: "bottom" },
-  { value: "4", label: "top, height" },
-  { value: "5", label: "top, bottom" },
-  { value: "6", label: "height, bottom" },
-];
-
-const gcHOptions = [
-  { value: "0", label: "none" },
-  { value: "1", label: "left" },
-  { value: "2", label: "width" },
-  { value: "3", label: "right" },
-  { value: "4", label: "left, width" },
-  { value: "5", label: "left, right" },
-  { value: "6", label: "width, right" },
-];
+const gcIcons = [gcIcon0, gcIcon1, gcIcon2, gcIcon3, gcIcon4, gcIcon5, gcIcon6];
+const gcVAlts = ["None", "Top", "Height", "Bottom", "Top & Height", "Top & Bottom", "Height & Bottom"];
+const gcHAlts = ["None", "Left", "Width", "Right", "Left & Width", "Left & Right", "Width & Right"];
 
 interface Props {
   targetShape: Shape;
@@ -31,43 +20,72 @@ interface Props {
 
 export const GroupConstraintInspector: React.FC<Props> = ({ targetShape, updateTargetShape }) => {
   const handleGcVChange = useCallback(
-    (val: string) => {
-      const gcV = parseInt(val) as GroupConstraint;
-      updateTargetShape({ gcV });
+    (val: GroupConstraint) => {
+      updateTargetShape({ gcV: val });
     },
     [updateTargetShape],
   );
 
   const handleGcHChange = useCallback(
-    (val: string) => {
-      const gcH = parseInt(val) as GroupConstraint;
-      updateTargetShape({ gcH });
+    (val: GroupConstraint) => {
+      updateTargetShape({ gcH: val });
     },
     [updateTargetShape],
   );
 
   return (
     <BlockGroupField label="Group constraints">
-      <InlineField label="Vertical fixed">
-        <div className="w-36">
-          <SelectInput
-            value={(targetShape.gcV ?? 0).toString()}
-            options={gcVOptions}
-            onChange={handleGcVChange}
-            keepFocus
-          />
+      <BlockField label="Vertical fixed">
+        <div className="w-56 flex flex-wrap justify-end gap-1">
+          {gcIcons.map((icon, i) => (
+            <ItemButton
+              key={icon}
+              value={i as any}
+              icon={icon}
+              selectedValue={targetShape.gcV}
+              vertical
+              onClick={handleGcVChange}
+            />
+          ))}
         </div>
-      </InlineField>
-      <InlineField label="Horizontal fixed">
-        <div className="w-36">
-          <SelectInput
-            value={(targetShape.gcH ?? 0).toString()}
-            options={gcHOptions}
-            onChange={handleGcHChange}
-            keepFocus
-          />
+      </BlockField>
+      <BlockField label="Horinzontal fixed">
+        <div className="w-56 flex flex-wrap justify-end gap-1">
+          {gcIcons.map((icon, i) => (
+            <ItemButton
+              key={icon}
+              value={i as any}
+              icon={icon}
+              selectedValue={targetShape.gcH}
+              onClick={handleGcHChange}
+            />
+          ))}
         </div>
-      </InlineField>
+      </BlockField>
     </BlockGroupField>
+  );
+};
+
+interface ItemButtonProps {
+  value: GroupConstraint;
+  icon: string;
+  vertical?: boolean;
+  selectedValue?: GroupConstraint;
+  onClick?: (val: GroupConstraint) => void;
+}
+
+export const ItemButton: React.FC<ItemButtonProps> = ({ value, icon, vertical, selectedValue, onClick }) => {
+  const handleClick = useCallback(() => {
+    onClick?.(value);
+  }, [value, onClick]);
+
+  const alt = (vertical ? gcVAlts : gcHAlts)[value];
+  const highlightClassName = value === (selectedValue ?? 0) ? " border-2 border-cyan-400" : "";
+
+  return (
+    <button type="button" onClick={handleClick} className={"relative w-12 h-12"}>
+      <div className={"absolute inset-0" + highlightClassName} />
+      <img src={icon} alt={alt} className={"absolute inset-0" + (vertical ? " rotate-90" : "")} />
+    </button>
   );
 };
