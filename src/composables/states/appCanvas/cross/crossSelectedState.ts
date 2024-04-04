b2782ae2db8b482341edea929bafd286da834d4c
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-04-04 12:09:45 +0900

    refactor: Use `defineSingleSelectedHandlerState`

--- src/composables/states/appCanvas/cross/crossSelectedState.ts
+++ src/composables/states/appCanvas/cross/crossSelectedState.ts
@@ -46,8 +46,6 @@ export const newCrossSelectedState = defineSingleSelectedHandlerState(
                           },
                         });
                       };
-                    default:
-                      return;
                   }
                 }
               }
--- src/composables/states/appCanvas/cross/diagonalCrossSelectedState.ts
+++ src/composables/states/appCanvas/cross/diagonalCrossSelectedState.ts
@@ -1,149 +1,58 @@
-import {
-  handleCommonPointerDownLeftOnSingleSelection,
-  handleCommonPointerDownRightOnSingleSelection,
-  handleIntransientEvent,
-} from "../commons";
-import { newSelectionHubState } from "../selectionHubState";
-import { CONTEXT_MENU_SHAPE_SELECTED_ITEMS } from "../contextMenuItems";
-import { BoundingBox, newBoundingBox } from "../../../boundingBox";
-import { newResizingState } from "../resizingState";
-import { newRotatingState } from "../rotatingState";
 import { DiagonalCrossShape } from "../../../../shapes/polygons/diagonalCross";
 import { newCrossHandler, renderMovingCrossAnchor } from "../../../shapeHandlers/crossHandler";
-import { defineIntransientState } from "../intransientState";
-import { newPointerDownEmptyState } from "../pointerDownEmptyState";
 import { movingShapeControlState } from "../movingShapeControlState";
 import { getShapeDetransform, getShapeTransform } from "../../../../shapes/simplePolygon";
 import { applyAffine, clamp } from "okageo";
 import { snapNumber } from "../../../../utils/geometry";
+import { defineSingleSelectedHandlerState } from "../singleSelectedHandlerState";
 
-export const newDiagonalCrossSelectedState = defineIntransientState(() => {
-  let targetShape: DiagonalCrossShape;
-  let shapeHandler: ReturnType<typeof newCrossHandler>;
-  let boundingBox: BoundingBox;
+export const newDiagonalCrossSelectedState = defineSingleSelectedHandlerState(
+  (getters) => {
+    return {
+      getLabel: () => "DiagonalCrossSelected",
+      handleEvent: (ctx, event) => {
+        switch (event.type) {
+          case "pointerdown":
+            switch (event.data.options.button) {
+              case 0: {
+                const targetShape = getters.getTargetShape();
+                const shapeHandler = getters.getShapeHandler();
 
-  return {
-    getLabel: () => "DiagonalCrossSelected",
-    onStart: (ctx) => {
-      ctx.showFloatMenu();
-      ctx.setCommandExams([]);
-      targetShape = ctx.getShapeComposite().shapeMap[ctx.getLastSelectedShapeId() ?? ""] as DiagonalCrossShape;
-      shapeHandler = newCrossHandler({ getShapeComposite: ctx.getShapeComposite, targetId: targetShape.id });
-
-      const shapeComposite = ctx.getShapeComposite();
-      boundingBox = newBoundingBox({
-        path: shapeComposite.getLocalRectPolygon(targetShape),
-      });
-    },
-    onEnd: (ctx) => {
-      ctx.hideFloatMenu();
-      ctx.setCommandExams();
-      ctx.setContextMenuList();
-      ctx.setCursor();
-    },
-    handleEvent: (ctx, event) => {
-      if (!targetShape) return newSelectionHubState;
-
-      switch (event.type) {
-        case "pointerdown":
-          ctx.setContextMenuList();
-
-          switch (event.data.options.button) {
-            case 0: {
-              const hitResult = shapeHandler.hitTest(event.data.point, ctx.getScale());
-              shapeHandler.saveHitResult(hitResult);
-              if (hitResult) {
-                switch (hitResult.type) {
-                  case "crossSize":
-                    return () => {
-                      let showLabel = false;
-                      return movingShapeControlState<DiagonalCrossShape>({
-                        targetId: targetShape.id,
-                        snapType: "custom",
-                        patchFn: (s, p, movement) => {
-                          const localP = applyAffine(getShapeDetransform(s), p);
-                          let nextSize = clamp(1, Math.min(s.width / 2, s.height / 2), localP.x - s.width / 2) * 2;
-                          if (movement.ctrl) {
-                            showLabel = false;
-                          } else {
-                            nextSize = snapNumber(nextSize, 1);
-                            showLabel = true;
-                          }
-                          return { crossSize: nextSize };
-                        },
-                        getControlFn: (s) =>
-                          applyAffine(getShapeTransform(s), { x: s.width / 2 + s.crossSize / 2, y: s.height / 2 }),
-                        renderFn: (ctx, renderCtx, s) => {
-                          renderMovingCrossAnchor(renderCtx, ctx.getStyleScheme(), ctx.getScale(), s, showLabel);
-                        },
-                      });
-                    };
-                  default:
-                    return;
+                const hitResult = shapeHandler.hitTest(event.data.point, ctx.getScale());
+                shapeHandler.saveHitResult(hitResult);
+                if (hitResult) {
+                  switch (hitResult.type) {
+                    case "crossSize":
+                      return () => {
+                        let showLabel = false;
+                        return movingShapeControlState<DiagonalCrossShape>({
+                          targetId: targetShape.id,
+                          snapType: "custom",
+                          patchFn: (s, p, movement) => {
+                            const localP = applyAffine(getShapeDetransform(s), p);
+                            let nextSize = clamp(1, Math.min(s.width / 2, s.height / 2), localP.x - s.width / 2) * 2;
+                            if (movement.ctrl) {
+                              showLabel = false;
+                            } else {
+                              nextSize = snapNumber(nextSize, 1);
+                              showLabel = true;
+                            }
+                            return { crossSize: nextSize };
+                          },
+                          getControlFn: (s) =>
+                            applyAffine(getShapeTransform(s), { x: s.width / 2 + s.crossSize / 2, y: s.height / 2 }),
+                          renderFn: (ctx, renderCtx, s) => {
+                            renderMovingCrossAnchor(renderCtx, ctx.getStyleScheme(), ctx.getScale(), s, showLabel);
+                          },
+                        });
+                      };
+                  }
                 }
               }
-
-              const boundingHitResult = boundingBox.hitTest(event.data.point, ctx.getScale());
-              if (boundingHitResult) {
-                switch (boundingHitResult.type) {
-                  case "corner":
-                  case "segment":
-                    return () => newResizingState({ boundingBox, hitResult: boundingHitResult });
-                  case "rotation":
-                    return () => newRotatingState({ boundingBox });
-                }
-              }
-
-              return handleCommonPointerDownLeftOnSingleSelection(
-                ctx,
-                event,
-                targetShape.id,
-                ctx.getShapeComposite().getSelectionScope(targetShape),
-              );
             }
-            case 1:
-              return () => newPointerDownEmptyState(event.data.options);
-            case 2: {
-              return handleCommonPointerDownRightOnSingleSelection(
-                ctx,
-                event,
-                targetShape.id,
-                ctx.getShapeComposite().getSelectionScope(targetShape),
-              );
-            }
-            default:
-              return;
-          }
-        case "pointerhover": {
-          const nextHitResult = shapeHandler.hitTest(event.data.current, ctx.getScale());
-          if (shapeHandler.saveHitResult(nextHitResult)) {
-            ctx.redraw();
-          }
-
-          if (nextHitResult) {
-            boundingBox.saveHitResult();
-            return;
-          }
-
-          const hitBounding = boundingBox.hitTest(event.data.current, ctx.getScale());
-          if (boundingBox.saveHitResult(hitBounding)) {
-            ctx.redraw();
-          }
-          break;
         }
-        case "contextmenu":
-          ctx.setContextMenuList({
-            items: CONTEXT_MENU_SHAPE_SELECTED_ITEMS,
-            point: event.data.point,
-          });
-          return;
-        default:
-          return handleIntransientEvent(ctx, event);
-      }
-    },
-    render: (ctx, renderCtx) => {
-      boundingBox.render(renderCtx, ctx.getStyleScheme(), ctx.getScale());
-      shapeHandler.render(renderCtx, ctx.getStyleScheme(), ctx.getScale());
-    },
-  };
-});
+      },
+    };
+  },
+  (ctx, target) => newCrossHandler({ getShapeComposite: ctx.getShapeComposite, targetId: target.id }),
+);
