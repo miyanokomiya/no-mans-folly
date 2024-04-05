c9ddcf7578999e8a96d1916a5cc8f3bb0b85f2b1
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-04-05 12:31:51 +0900

    refactor: Use `defineSingleSelectedHandlerState`

--- src/composables/states/appCanvas/trapezoid/trapezoidSelectedState.ts
+++ src/composables/states/appCanvas/trapezoid/trapezoidSelectedState.ts
@@ -1,126 +1,37 @@
-import {
-  handleCommonPointerDownLeftOnSingleSelection,
-  handleCommonPointerDownRightOnSingleSelection,
-  handleIntransientEvent,
-} from "../commons";
-import { newSelectionHubState } from "../selectionHubState";
-import { CONTEXT_MENU_SHAPE_SELECTED_ITEMS } from "../contextMenuItems";
-import { BoundingBox, newBoundingBox } from "../../../boundingBox";
-import { newResizingState } from "../resizingState";
-import { newRotatingState } from "../rotatingState";
-import { TrapezoidShape } from "../../../../shapes/polygons/trapezoid";
 import { newTrapezoidHandler } from "../../../shapeHandlers/trapezoidHandler";
 import { newTransformingTrapezoidState } from "./transformingTrapezoidState";
-import { defineIntransientState } from "../intransientState";
-import { newPointerDownEmptyState } from "../pointerDownEmptyState";
+import { defineSingleSelectedHandlerState } from "../singleSelectedHandlerState";
 
-export const newTrapezoidSelectedState = defineIntransientState(() => {
-  let targetShape: TrapezoidShape;
-  let shapeHandler: ReturnType<typeof newTrapezoidHandler>;
-  let boundingBox: BoundingBox;
+export const newTrapezoidSelectedState = defineSingleSelectedHandlerState(
+  (getters) => {
+    return {
+      getLabel: () => "TrapezoidSelected",
+      handleEvent: (ctx, event) => {
+        switch (event.type) {
+          case "pointerdown":
+            ctx.setContextMenuList();
 
-  return {
-    getLabel: () => "TrapezoidSelected",
-    onStart: (ctx) => {
-      ctx.showFloatMenu();
-      ctx.setCommandExams([]);
-      targetShape = ctx.getShapeComposite().shapeMap[ctx.getLastSelectedShapeId() ?? ""] as TrapezoidShape;
-      shapeHandler = newTrapezoidHandler({ getShapeComposite: ctx.getShapeComposite, targetId: targetShape.id });
-
-      const shapeComposite = ctx.getShapeComposite();
-      boundingBox = newBoundingBox({
-        path: shapeComposite.getLocalRectPolygon(targetShape),
-      });
-    },
-    onEnd: (ctx) => {
-      ctx.hideFloatMenu();
-      ctx.setCommandExams();
-      ctx.setContextMenuList();
-      ctx.setCursor();
-    },
-    handleEvent: (ctx, event) => {
-      if (!targetShape) return newSelectionHubState;
-
-      switch (event.type) {
-        case "pointerdown":
-          ctx.setContextMenuList();
-
-          switch (event.data.options.button) {
-            case 0: {
-              const hitResult = shapeHandler.hitTest(event.data.point, ctx.getScale());
-              shapeHandler.saveHitResult(hitResult);
-              if (hitResult) {
-                switch (hitResult.type) {
-                  case "c0":
-                  case "c1":
-                    return () =>
-                      newTransformingTrapezoidState({ targetId: targetShape.id, controlKey: hitResult.type });
-                  default:
-                    return;
+            switch (event.data.options.button) {
+              case 0: {
+                const targetShape = getters.getTargetShape();
+                const shapeHandler = getters.getShapeHandler();
+                const hitResult = shapeHandler.hitTest(event.data.point, ctx.getScale());
+                shapeHandler.saveHitResult(hitResult);
+                if (hitResult) {
+                  switch (hitResult.type) {
+                    case "c0":
+                    case "c1":
+                      return () =>
+                        newTransformingTrapezoidState({ targetId: targetShape.id, controlKey: hitResult.type });
+                    default:
+                      return;
+                  }
                 }
               }
-
-              const boundingHitResult = boundingBox.hitTest(event.data.point, ctx.getScale());
-              if (boundingHitResult) {
-                switch (boundingHitResult.type) {
-                  case "corner":
-                  case "segment":
-                    return () => newResizingState({ boundingBox, hitResult: boundingHitResult });
-                  case "rotation":
-                    return () => newRotatingState({ boundingBox });
-                }
-              }
-
-              return handleCommonPointerDownLeftOnSingleSelection(
-                ctx,
-                event,
-                targetShape.id,
-                ctx.getShapeComposite().getSelectionScope(targetShape),
-              );
             }
-            case 1:
-              return () => newPointerDownEmptyState(event.data.options);
-            case 2: {
-              return handleCommonPointerDownRightOnSingleSelection(
-                ctx,
-                event,
-                targetShape.id,
-                ctx.getShapeComposite().getSelectionScope(targetShape),
-              );
-            }
-            default:
-              return;
-          }
-        case "pointerhover": {
-          const nextHitResult = shapeHandler.hitTest(event.data.current, ctx.getScale());
-          if (shapeHandler.saveHitResult(nextHitResult)) {
-            ctx.redraw();
-          }
-
-          if (nextHitResult) {
-            boundingBox.saveHitResult();
-            return;
-          }
-
-          const hitBounding = boundingBox.hitTest(event.data.current, ctx.getScale());
-          if (boundingBox.saveHitResult(hitBounding)) {
-            ctx.redraw();
-          }
-          break;
         }
-        case "contextmenu":
-          ctx.setContextMenuList({
-            items: CONTEXT_MENU_SHAPE_SELECTED_ITEMS,
-            point: event.data.point,
-          });
-          return;
-        default:
-          return handleIntransientEvent(ctx, event);
-      }
-    },
-    render: (ctx, renderCtx) => {
-      boundingBox.render(renderCtx, ctx.getStyleScheme(), ctx.getScale());
-      shapeHandler.render(renderCtx, ctx.getStyleScheme(), ctx.getScale());
-    },
-  };
-});
+      },
+    };
+  },
+  (ctx, target) => newTrapezoidHandler({ getShapeComposite: ctx.getShapeComposite, targetId: target.id }),
+);
