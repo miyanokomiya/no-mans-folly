d434dfc468e9ff39550fa77b3680dcded77395a8
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-04-20 11:20:52 +0900

    fix: Use inscribed rectangle of the inner ellipse as the text bounds of star shape

--- src/shapes/polygons/star.ts
+++ src/shapes/polygons/star.ts
@@ -1,4 +1,4 @@
-import { IVec2, applyAffine, clamp, multiAffines } from "okageo";
+import { IVec2, clamp } from "okageo";
 import { ShapeStruct, createBaseShape } from "../core";
 import {
   SimplePath,
@@ -35,23 +35,20 @@ export const struct: ShapeStruct<StarShape> = {
   },
   getTextRangeRect(shape) {
     return getSimpleShapeTextRangeRect(shape, (s) => {
+      // Get inscribed rectangle of the inner ellipse.
       const c = { x: s.width / 2, y: s.height / 2 };
       const r = Math.PI / 4;
       const rate = getRadiusRate(s);
-      const dx = Math.cos(r) * s.width * rate;
-      const dy = Math.sin(r) * s.height * rate;
-      const rect = { x: c.x - dx, y: c.y - dy, width: dx * 2, height: dy * 2 };
+      const gapRate = getSizeGapScale(s);
+      const dx = Math.cos(r) * s.width * rate * gapRate.x;
+      const dy = Math.sin(r) * s.height * rate * gapRate.y;
 
-      const gapRate = getSizeGapRate(s);
-      const gapW = rect.width * gapRate.x;
-      const adjusted = {
-        x: rect.x - gapW / 2,
-        y: rect.y * (1 + gapRate.y),
-        width: rect.width + gapW,
-        height: rect.height * (1 + gapRate.y),
+      return {
+        x: c.x - dx + s.p.x,
+        y: c.y * gapRate.y - dy + s.p.y,
+        width: dx * 2,
+        height: dy * 2,
       };
-
-      return { x: adjusted.x + s.p.x, y: adjusted.y + s.p.y, width: adjusted.width, height: adjusted.height };
     });
   },
   canAttachSmartBranch: true,
@@ -73,19 +70,23 @@ function getRawPath(shape: StarShape): SimplePath {
   const size = getSize(shape);
   const unitR = (Math.PI * 2) / size;
   const arr = [...Array(size)].map((_, i) => i);
+  const gapRate = getSizeGapScale(shape);
+  const width = shape.width * gapRate.x;
+  const height = shape.height * gapRate.y;
+  const c = { x: shape.width / 2, y: height / 2 };
 
-  const c = { x: shape.width / 2, y: shape.height / 2 };
+  const outerRadius = { x: width / 2, y: height / 2 };
   const ops = arr.map<IVec2>((i) => {
     const r = unitR * i - Math.PI / 2;
-    return { x: Math.cos(r) * c.x, y: Math.sin(r) * c.y };
+    return { x: Math.cos(r) * outerRadius.x + c.x, y: Math.sin(r) * outerRadius.y + c.y };
   });
 
   const rate = getRadiusRate(shape);
-  const innerRadius = { x: shape.width * rate, y: shape.height * rate };
+  const innerRadius = { x: width * rate, y: height * rate };
   const indexDiff = Math.floor(size / 2);
   const ips = arr.map<IVec2>((i) => {
     const r = unitR * (i - indexDiff) + Math.PI / 2;
-    return { x: Math.cos(r) * innerRadius.x, y: Math.sin(r) * innerRadius.y };
+    return { x: Math.cos(r) * innerRadius.x + c.x, y: Math.sin(r) * innerRadius.y + c.y };
   });
 
   const path: IVec2[] = [];
@@ -93,22 +94,16 @@ function getRawPath(shape: StarShape): SimplePath {
     path.push(ops[i], ips[i]);
   }
 
-  const gapX = c.x + Math.min(...path.map((p) => p.x));
-  const gapY = c.y - Math.max(...path.map((p) => p.y));
-  const affine = multiAffines([
-    [1, 0, 0, 1, c.x, 0],
-    [1 + (2 * gapX) / shape.width, 0, 0, 1 + gapY / shape.height, 0, 0],
-    [1, 0, 0, 1, 0, c.y],
-  ]);
-
-  return { path: path.map((p) => applyAffine(affine, p)) };
+  return { path };
 }
 
-function getSizeGapRate(shape: StarShape): IVec2 {
+function getSizeGapScale(shape: StarShape): IVec2 {
   const size = getSize(shape);
   const unitR = (Math.PI * 2) / size;
   const from = -Math.PI / 2;
   const bottomIndex = Math.ceil(size / 2);
+  // Avoid picking 0 when size is 3.
   const rightIndex = Math.max(1, Math.floor(size / 4));
-  return { x: (1 - Math.cos(from + unitR * rightIndex)) * 2, y: 1 - Math.sin(from + unitR * bottomIndex) };
+  // Each side expect for top can have gap because this shape is based on the top.
+  return { x: 1 / Math.cos(from + unitR * rightIndex), y: 1 / ((1 + Math.sin(from + unitR * bottomIndex)) / 2) };
 }
