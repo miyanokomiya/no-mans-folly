9a7a0c90490a44866e353f9391bb76aba9d7c6db
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-06-06 12:53:17 +0900

    feat: Allow elbow segment snap to other shapes

--- src/composables/states/appCanvas/lines/movingElbowSegmentState.ts
+++ src/composables/states/appCanvas/lines/movingElbowSegmentState.ts
@@ -1,13 +1,12 @@
 import type { AppCanvasState } from "../core";
 import { handleHistoryEvent } from "../commons";
-import { LineShape, getEdges, getLinePath, patchBodyVertex } from "../../../../shapes/line";
-import { getDistance, getInner, getPedal, sub } from "okageo";
+import { LineShape, getEdges, getLinePath, isLineShape, patchBodyVertex } from "../../../../shapes/line";
+import { MINVALUE, add, getDistance, getInner, getOuterRectangle, getPedal, getRadian, moveRect, sub } from "okageo";
 import { newSelectionHubState } from "../selectionHubState";
-import { scaleGlobalAlpha } from "../../../../utils/renderer";
 import { getPatchAfterLayouts } from "../../../shapeLayoutHandler";
 import { COMMAND_EXAM_SRC } from "../commandExams";
-import { applyStrokeStyle } from "../../../../utils/strokeStyle";
 import { ElbowLineHandler, newElbowLineHandler } from "../../../elbowLineHandler";
+import { ShapeSnapping, SnappingResult, newShapeSnapping, renderSnappingResult } from "../../../shapeSnapping";
 
 interface Option {
   lineShape: LineShape;
@@ -18,6 +17,8 @@ export function newMovingElbowSegmentState(option: Option): AppCanvasState {
   const targetSegment = getEdges(option.lineShape)[option.index];
   const srcBodyItem = option.lineShape.body?.[option.index - 1];
   let elbowHandler: ElbowLineHandler;
+  let shapeSnapping: ShapeSnapping;
+  let snappingResult: SnappingResult | undefined;
 
   return {
     getLabel: () => "MovingElbowSegment",
@@ -27,6 +28,23 @@ export function newMovingElbowSegmentState(option: Option): AppCanvasState {
       ctx.startDragging();
       ctx.setCommandExams([COMMAND_EXAM_SRC.DISABLE_SNAP]);
       elbowHandler = newElbowLineHandler(ctx);
+
+      const shapeComposite = ctx.getShapeComposite();
+      const snappableShapes = shapeComposite.getShapesOverlappingRect(
+        Object.values(shapeComposite.shapeMap).filter((s) => !isLineShape(s)),
+        ctx.getViewRect(),
+      );
+
+      // Target elbow segment is always cardinal, so either horizontal or vertical snapping is available at once.
+      const isHorizontalSegment = Math.abs(Math.sin(getRadian(targetSegment[1], targetSegment[0]))) < MINVALUE;
+      shapeSnapping = newShapeSnapping({
+        shapeSnappingList: snappableShapes.map((s) => {
+          const lines = shapeComposite.getSnappingLines(s);
+          return [s.id, isHorizontalSegment ? { v: [], h: lines.h } : { v: lines.v, h: [] }];
+        }),
+        scale: ctx.getScale(),
+        gridSnapping: ctx.getGrid().getSnappingLines(),
+      });
     },
     onEnd: (ctx) => {
       ctx.stopDragging();
@@ -38,7 +56,13 @@ export function newMovingElbowSegmentState(option: Option): AppCanvasState {
 
       switch (event.type) {
         case "pointermove": {
-          const p = event.data.current;
+          snappingResult = event.data.ctrl
+            ? undefined
+            : shapeSnapping.test(
+                moveRect(getOuterRectangle([targetSegment]), sub(event.data.current, event.data.start)),
+              );
+          const p = snappingResult ? add(event.data.current, snappingResult.diff) : event.data.current;
+
           const elbow = srcBodyItem.elbow;
           const pedal = getPedal(p, targetSegment);
 
@@ -77,14 +101,18 @@ export function newMovingElbowSegmentState(option: Option): AppCanvasState {
       }
     },
     render(ctx, renderCtx) {
-      const style = ctx.getStyleScheme();
-      applyStrokeStyle(renderCtx, { color: style.selectionPrimary });
-      scaleGlobalAlpha(renderCtx, 0.5, () => {
-        renderCtx.beginPath();
-        renderCtx.moveTo(targetSegment[0].x, targetSegment[0].y);
-        renderCtx.lineTo(targetSegment[1].x, targetSegment[1].y);
-        renderCtx.stroke();
-      });
+      if (snappingResult) {
+        const shapeComposite = ctx.getShapeComposite();
+        const shapeMap = shapeComposite.shapeMap;
+        const scale = ctx.getScale();
+        const style = ctx.getStyleScheme();
+        renderSnappingResult(renderCtx, {
+          style,
+          scale,
+          result: snappingResult,
+          getTargetRect: (id) => shapeComposite.getWrapperRect(shapeMap[id]),
+        });
+      }
     },
   };
 }
