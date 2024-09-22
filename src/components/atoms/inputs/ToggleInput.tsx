8102baee528b0c6eee163d0cee304cb7ebb35609
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-09-22 19:49:21 +0900

    style: Show a checkmark for checked field

--- src/components/atoms/inputs/ToggleInput.tsx
+++ src/components/atoms/inputs/ToggleInput.tsx
@@ -1,4 +1,5 @@
 import { useCallback } from "react";
+import iconCheck from "../../../assets/icons/check.svg";
 
 interface Props {
   value?: boolean;
@@ -13,7 +14,9 @@ export const ToggleInput: React.FC<Props> = ({ value, onChange, children }) => {
 
   return (
     <button type="button" onClick={onClick} className="flex items-center gap-2">
-      <div className={"w-4 h-4 border border-black rounded " + (value ? "bg-blue-500" : "")}></div>
+      <div className={"w-4 h-4 border border-black rounded " + (value ? "bg-blue-500" : "")}>
+        {value ? <img src={iconCheck} alt="" /> : undefined}
+      </div>
       {children}
     </button>
   );
