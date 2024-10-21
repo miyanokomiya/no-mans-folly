166f0a66f2d1f48220474a9074af2130f48907ea
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-10-21 13:05:00 +0900

    feat: Introduce vite-plugin-pwa to make the app enable offline
    - Just placing the manifest wasn't enough for that purpose.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import handlebars from "vite-plugin-handlebars";
+import { VitePWA } from "vite-plugin-pwa";
 
 const root = resolve(__dirname, "src", "pages");
 const outDir = resolve(__dirname, "dist");
@@ -26,6 +27,44 @@ export default defineConfig({
       partialDirectory: resolve(__dirname, "src", "partials"),
       context: {
         title: "No-man's folly",
+      },
+    }),
+    VitePWA({
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      includeAssets: ["**/*"],
+      manifest: {
+        theme_color: "#bfbfbf",
+        background_color: "#bfbfbf",
+        display: "standalone",
+        orientation: "landscape",
+        scope: "/",
+        start_url: "/",
+        name: "No-man's folly",
+        short_name: "No-man's folly",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
     }),
   ],
