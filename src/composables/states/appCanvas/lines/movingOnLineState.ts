8534d010d2f2b572570044c91164dfd168352258
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-11-02 00:22:23 +0900

    refactor: Needless to manually calculate attached points in MovingOnLine state
    - It's done via `getPatchAfterLayouts`

--- src/composables/states/appCanvas/lines/movingOnLineState.ts
+++ src/composables/states/appCanvas/lines/movingOnLineState.ts
@@ -2,10 +2,10 @@ import type { AppCanvasState } from "../core";
 import { applyFillStyle } from "../../../../utils/fillStyle";
 import { mapReduce, patchPipe, toList } from "../../../../utils/commons";
 import { getLinePath, isLineShape, LineShape } from "../../../../shapes/line";
-import { getClosestOutlineInfoOfLine, getLineEdgeInfo } from "../../../../shapes/utils/line";
+import { getClosestOutlineInfoOfLine } from "../../../../shapes/utils/line";
 import { TAU } from "../../../../utils/geometry";
 import { add, IVec2, lerpPoint, sub } from "okageo";
-import { getAttachmentAnchorPoint, patchByMoveToAttachedPoint } from "../../../lineAttachmentHandler";
+import { getAttachmentAnchorPoint } from "../../../lineAttachmentHandler";
 import { ShapeAttachment } from "../../../../models";
 import { applyCurvePath } from "../../../../utils/renderer";
 import { applyStrokeStyle } from "../../../../utils/strokeStyle";
@@ -20,12 +20,10 @@ export function newMovingOnLineState(option: Option): AppCanvasState {
   let keepMoving = false;
   let lineAnchor: IVec2 | undefined;
   let line: LineShape;
-  let lineLerpFn: (t: number) => IVec2;
 
   return {
     getLabel: () => "MovingOnLine",
     onStart: (ctx) => {
-      // ctx.setTmpShapeMap({});
       const shapeComposite = ctx.getShapeComposite();
       const shapeMap = shapeComposite.shapeMap;
       line = shapeMap[option.lineId] as LineShape;
@@ -33,8 +31,6 @@ export function newMovingOnLineState(option: Option): AppCanvasState {
         keepMoving = true;
         return { type: "break" };
       }
-
-      lineLerpFn = getLineEdgeInfo(line).lerpFn;
     },
     onEnd: (ctx) => {
       if (keepMoving) {
@@ -66,13 +62,13 @@ export function newMovingOnLineState(option: Option): AppCanvasState {
             return { type: "break" };
           }
 
+          const baseTo = { x: closestInfo[1], y: 0 };
           const baseToP = closestInfo[0];
           lineAnchor = baseToP;
-          const baseTo = { x: closestInfo[1], y: 0 };
 
           const selectedShapeMap = mapReduce(ctx.getSelectedShapeIdMap(), (_, id) => shapeMap[id]);
           const selectedShapes = toList(selectedShapeMap);
-          const attachInfoMap = new Map<string, [to: IVec2, toP: IVec2]>([[option.shapeId, [baseTo, baseToP]]]);
+          const attachInfoMap = new Map<string, [to: IVec2]>([[option.shapeId, [baseTo]]]);
 
           if (selectedShapes.length > 1) {
             const toLerpFn = (t: number) => lerpPoint(baseTo, { x: 1, y: 0 }, t);
@@ -81,7 +77,7 @@ export function newMovingOnLineState(option: Option): AppCanvasState {
               .filter((s) => s.id !== option.shapeId)
               .forEach((s, i) => {
                 const to = toLerpFn(step * (i + 1));
-                attachInfoMap.set(s.id, [to, lineLerpFn(to.x)]);
+                attachInfoMap.set(s.id, [to]);
               });
           }
 
@@ -98,10 +94,7 @@ export function newMovingOnLineState(option: Option): AppCanvasState {
                     id: line.id,
                     to: info[0],
                   };
-                  return {
-                    ...patchByMoveToAttachedPoint(shapeComposite, s, attachment.anchor, info[1]),
-                    attachment,
-                  };
+                  return { attachment };
                 });
               },
               (_, currentPatch) => {
