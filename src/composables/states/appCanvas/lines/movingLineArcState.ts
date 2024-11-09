e07db88857630609add607ac685682700592ecd7
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-11-10 00:18:26 +0900

    fix: Regard zero length edge when arc control is modified

--- src/composables/states/appCanvas/lines/movingLineArcState.ts
+++ src/composables/states/appCanvas/lines/movingLineArcState.ts
@@ -1,6 +1,6 @@
 import type { AppCanvasState } from "../core";
 import { LineShape, getEdges } from "../../../../shapes/line";
-import { IVec2, add, getCenter, getRadian, rotate, sub } from "okageo";
+import { IVec2, add, getCenter, getRadian, isSame, rotate, sub } from "okageo";
 import { applyFillStyle } from "../../../../utils/fillStyle";
 import { COMMAND_EXAM_SRC } from "../commandExams";
 import { TAU, getRotateFn } from "../../../../utils/geometry";
@@ -16,6 +16,7 @@ import { applyStrokeStyle } from "../../../../utils/strokeStyle";
 import { applyPath } from "../../../../utils/renderer";
 import { fillArray } from "../../../../utils/commons";
 import { newCoordinateRenderer } from "../../../coordinateRenderer";
+import { isArcControl } from "../../../../utils/path";
 
 interface Option {
   lineShape: LineShape;
@@ -25,7 +26,16 @@ interface Option {
 
 export function newMovingLineArcState(option: Option): AppCanvasState {
   const edge = getEdges(option.lineShape)[option.index];
-  const rotateFn = getRotateFn(getRadian(edge[1], edge[0]));
+
+  const zeroEdge = isSame(edge[0], edge[1]);
+  const currentCurve = option.lineShape.curves?.[option.index];
+  const edgeRotation = zeroEdge
+    ? isArcControl(currentCurve)
+      ? getRadian(currentCurve.d) + Math.PI / 2
+      : 0
+    : getRadian(edge[1], edge[0]);
+
+  const rotateFn = getRotateFn(edgeRotation);
   let shapeSnapping: VectorSnapping;
   let snappingResult: VectorSnappingResult | undefined;
   const coordinateRenderer = newCoordinateRenderer({ coord: option.p });
@@ -43,16 +53,19 @@ export function newMovingLineArcState(option: Option): AppCanvasState {
         Object.values(shapeMap).filter((s) => s.id !== option.lineShape.id),
         ctx.getViewRect(),
       );
+
       shapeSnapping = newVectorSnapping({
         origin: getCenter(edge[1], edge[0]),
-        vector: rotate(sub(edge[1], edge[0]), -Math.PI / 2),
+        vector: zeroEdge
+          ? rotate({ x: 1, y: 0 }, edgeRotation - Math.PI / 2)
+          : rotate(sub(edge[1], edge[0]), -Math.PI / 2),
         snappableShapes,
         gridSnapping: ctx.getGrid().getSnappingLines(),
         getShapeStruct: shapeComposite.getShapeStruct,
         snappableOrigin: true,
       });
 
-      const d = rotateFn(sub(option.p, edge[0]), true);
+      const d = zeroEdge ? sub(option.p, edge[0]) : rotateFn(sub(option.p, edge[0]), true);
       const curves = fillArray(option.index + 1, undefined, option.lineShape.curves);
       curves[option.index] = { d };
       ctx.setTmpShapeMap(
@@ -73,7 +86,7 @@ export function newMovingLineArcState(option: Option): AppCanvasState {
 
           snappingResult = event.data.ctrl ? undefined : shapeSnapping.hitTest(point, ctx.getScale());
           const p = snappingResult?.p ?? point;
-          const d = rotateFn(sub(p, edge[0]), true);
+          const d = zeroEdge ? sub(p, edge[0]) : rotateFn(sub(p, edge[0]), true);
           coordinateRenderer.saveCoord(p);
 
           const curves = fillArray(option.index + 1, undefined, option.lineShape.curves);
@@ -109,7 +122,7 @@ export function newMovingLineArcState(option: Option): AppCanvasState {
 
       const line: LineShape = { ...option.lineShape, ...ctx.getTmpShapeMap()[option.lineShape.id] };
       const d = (line.curves?.[option.index] as ArcCurveControl | undefined)?.d ?? { x: 0, y: 0 };
-      const p = add(rotateFn({ x: 0, y: d.y }), getCenter(edge[0], edge[1]));
+      const p = zeroEdge ? add(d, edge[0]) : add(rotateFn({ x: 0, y: d.y }), getCenter(edge[0], edge[1]));
 
       renderCtx.beginPath();
       renderCtx.arc(p.x, p.y, vertexSize, 0, TAU);
