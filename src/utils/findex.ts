2df935e051cffca602c9714eba31b5c402d38eda
miyanokomiya <tomoya.hstexze24632@gmail.com>
2024-11-23 14:33:04 +0900

    fix: Avoid using `generateKeyBetween` directly

--- src/utils/findex.ts
+++ src/utils/findex.ts
@@ -11,8 +11,8 @@ const _generateNKeysBetween = process.env.VITEST
  * Besides, when two values are same, returns the value.
  * It's possible enough that different entities have the same findex value under CRDT conversion situation.
  */
-export function generateKeyBetweenAllowSame(a: string | null, b: string | null): string {
-  return a && b && a === b ? a : findexing.generateKeyBetween(a, b);
+export function generateKeyBetweenAllowSame(a: string | null | undefined, b: string | null | undefined): string {
+  return a && b && a === b ? a : _generateKeyBetween(a ?? null, b ?? null);
 }
 
 export function generateKeyBetween(a: string | null | undefined, b: string | null | undefined) {
