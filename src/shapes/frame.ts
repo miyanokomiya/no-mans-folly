08865dfe0a19fefa9359e4b7598c6d5cf436bfaa
miyanokomiya <tomoya.hstexze24632@gmail.com>
2025-01-09 12:24:44 +0900

    feat: Show the name of the frame shape
    - This name isn't intended to be a part of the shape, so it's not exported in PNG or SVG

--- src/shapes/frame.ts
+++ src/shapes/frame.ts
@@ -1,6 +1,8 @@
 import { CommonStyle, Shape } from "../models";
-import { createFillStyle } from "../utils/fillStyle";
-import { createStrokeStyle } from "../utils/strokeStyle";
+import { COLORS } from "../utils/color";
+import { applyFillStyle, createFillStyle } from "../utils/fillStyle";
+import { applyDefaultTextStyle } from "../utils/renderer";
+import { applyStrokeStyle, createStrokeStyle } from "../utils/strokeStyle";
 import { ShapeStruct, createBaseShape } from "./core";
 import { RectPolygonShape } from "./rectPolygon";
 import { struct as rectangleStruct } from "./rectangle";
@@ -20,6 +22,15 @@ export const struct: ShapeStruct<FrameShape> = {
       height: arg.height ?? 300,
       name: arg.name ?? "new frame",
     };
+  },
+  render(ctx, shape) {
+    rectangleStruct.render(ctx, shape);
+    applyDefaultTextStyle(ctx, 18);
+    ctx.textBaseline = "bottom";
+    applyStrokeStyle(ctx, { color: COLORS.WHITE });
+    ctx.strokeText(shape.name, shape.p.x, shape.p.y - 2);
+    applyFillStyle(ctx, { color: COLORS.BLACK });
+    ctx.fillText(shape.name, shape.p.x, shape.p.y - 2);
   },
   resize(shape, resizingAffine) {
     const ret = rectangleStruct.resize(shape, resizingAffine);
