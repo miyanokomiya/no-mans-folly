03227fa28a44610c9dde411a75a7cfabcc62e1ed
miyanokomiya <tomoya.hstexze24632@gmail.com>
2025-01-19 19:09:02 +0900

    fix: Polish curly bracket appearance

--- src/shapes/polygons/curlyBracket.ts
+++ src/shapes/polygons/curlyBracket.ts
@@ -34,39 +34,35 @@ function getPath(shape: CurlyBracketShape): SimplePath {
   const cy = h / 2;
   const thickness = getBracketThickness(shape);
   const x1 = 0.2 * w;
+  const x2 = x1 + 0.1 * w;
   const y1 = 0.1 * h;
-  const x2 = 0.1 * w;
   const y2 = 0.3 * h;
-  const x3 = x2;
   const y3 = 0.4 * h;
 
-  const cx1 = 0.5 * w;
-  const cx2 = 0.3 * w;
+  const cx1 = x2 + 0.1 * w;
+  const cx2 = cx1 + 0.2 * w;
   const cy2 = 0.05 * h;
-  const cx3 = x2;
   const cy3 = 0.15 * h;
-  const cx4 = x3;
   const cy4 = 0.25 * h;
-
   const outerBeak = { x: 0, y: cy };
-  const innerBeak = { x: x3 + thickness * 0.75, y: cy };
 
   const outerTopPath = {
     path: [
       { x: w, y: 0 },
-      { x: x1, y: y1 },
-      { x: x2, y: y2 },
-      { x: x3, y: y3 },
+      { x: x2, y: y1 },
+      { x: x1, y: y2 },
+      { x: x1, y: y3 },
     ],
     curves: [
-      { c1: { x: cx1, y: 0 }, c2: { x: cx2, y: cy2 } },
-      { c1: { x: cx3, y: cy3 }, c2: { x: cx4, y: cy4 } },
-      { c1: { x: x2, y: y2 }, c2: { x: x3, y: y3 } },
-      { c1: { x: cx4, y: cy }, c2: outerBeak },
+      { c1: { x: cx2, y: 0 }, c2: { x: cx1, y: cy2 } },
+      { c1: { x: x1, y: cy3 }, c2: { x: x1, y: cy4 } },
+      { c1: { x: x1, y: y2 }, c2: { x: x1, y: y3 } },
+      { c1: { x: x1, y: cy }, c2: outerBeak },
     ],
   };
   const outerBottomPath = reverseBezierPath(flipBezierPathV(outerTopPath, cy));
 
+  const ix1 = x1 + thickness;
   const iy0 = thickness;
   const iy1 = Math.max(y1 + thickness * 0.6, iy0);
   const iy2 = Math.max(y2, iy1);
@@ -75,18 +71,20 @@ function getPath(shape: CurlyBracketShape): SimplePath {
   const icy1 = Math.max(cy2 + thickness * 0.6, iy0);
   const icy2 = clamp(iy1, iy2, cy3 + thickness * 0.6);
   const icy3 = clamp(icy2, iy2, cy4);
+  const innerBeak = { x: ix1 - thickness * 0.5, y: cy };
+
   const innerTopPath = reverseBezierPath({
     path: [
       { x: w, y: iy0 },
-      { x: x1 + thickness, y: iy1 },
-      { x: x2 + thickness, y: iy2 },
-      { x: x3 + thickness, y: iy3 },
+      { x: x2 + thickness, y: iy1 },
+      { x: ix1, y: iy2 },
+      { x: ix1, y: iy3 },
     ],
     curves: [
-      { c1: { x: cx1 + thickness * 0.6, y: icy0 }, c2: { x: cx2 + thickness, y: icy1 } },
-      { c1: { x: cx3 + thickness, y: icy2 }, c2: { x: cx4 + thickness, y: icy3 } },
-      { c1: { x: x2 + thickness, y: iy2 }, c2: { x: x3 + thickness, y: iy3 } },
-      { c1: { x: cx4 + thickness, y: cy }, c2: innerBeak },
+      { c1: { x: cx2 + thickness * 0.6, y: icy0 }, c2: { x: cx1 + thickness, y: icy1 } },
+      { c1: { x: x1 + thickness, y: icy2 }, c2: { x: x1 + thickness, y: icy3 } },
+      { c1: { x: x1 + thickness, y: iy2 }, c2: { x: x1 + thickness, y: iy3 } },
+      { c1: { x: x1 + thickness, y: cy }, c2: innerBeak },
     ],
   });
   const innerBottomPath = reverseBezierPath(flipBezierPathV(innerTopPath, cy));
