2352c6afec933c09a63078ee111dd8cf14f4b049
miyanokomiya <tomoya.hstexze24632@gmail.com>
2025-02-26 21:36:46 +0900

    fix: Fix test up-to-date

--- src/shapes/line.spec.ts
+++ src/shapes/line.spec.ts
@@ -228,8 +228,8 @@ describe("struct", () => {
 
     test("should adjust threshold via scale", () => {
       const shape = struct.create({ p: { x: 0, y: 0 }, q: { x: 10, y: 0 } });
-      expect(struct.isPointOn(shape, { x: 5, y: 4.5 }, undefined, 1)).toBe(false);
-      expect(struct.isPointOn(shape, { x: 5, y: 4.5 }, undefined, 2)).toBe(true);
+      expect(struct.isPointOn(shape, { x: 5, y: 6.5 }, undefined, 1)).toBe(false);
+      expect(struct.isPointOn(shape, { x: 5, y: 6.5 }, undefined, 2)).toBe(true);
     });
 
     test("should return true when the point is on a head", () => {
