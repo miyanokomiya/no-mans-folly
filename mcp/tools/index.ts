02ab1160bc015af468d485095d4ec371425f1e66
miyanokomiya <tomoya.hstexze24632@gmail.com>
2025-03-28 16:08:40 +0900

    feat: Add a tool to add a shape

--- mcp/tools/index.ts
+++ mcp/tools/index.ts
@@ -1,6 +1,6 @@
 import { ToolCallback } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { z, ZodRawShape } from "zod";
-import type { Context } from "../context";
+import type { Context } from "../context.js";
 
 type ToolStruct<Args extends ZodRawShape | undefined = undefined> = {
   name: string;
@@ -31,14 +31,35 @@ export function closeApp(): ToolStruct {
   };
 }
 
-const paramsSchema = { a: z.number(), b: z.number() };
-export function aaopenApp(): ToolStruct<typeof paramsSchema> {
+const addShapeParam = { point: z.object({ x: z.number(), y: z.number() }) };
+export function addShape(): ToolStruct<typeof addShapeParam> {
   return {
-    name: "open_app",
-    description: "Open No-man's folly application",
-    paramsSchema,
-    cb: async (ctx, { a, b }) => ({
-      content: [{ type: "text", text: String(a + b) }],
-    }),
+    name: "add_shape",
+    description: "Add new shape",
+    paramsSchema: addShapeParam,
+    cb: async (ctx, { point }) => {
+      const page = ctx.existingPage();
+      const id = await page.evaluate(appAddShape, { point });
+
+      return {
+        content: [{ type: "text", text: id ? `Create shape id is "${id}"` : "No shape was added" }],
+      };
+    },
   };
+}
+
+async function appAddShape(props: { point: { x: number; y: number } }): Promise<string> {
+  const app = (window as any).no_mans_folly;
+  if (!app) return "";
+
+  const accxt = app.getStateContext();
+  const shape = app.createShape(accxt.getShapeStruct, "rectangle", {
+    id: accxt.generateUuid(),
+    findex: accxt.createLastIndex(),
+    p: props.point,
+  });
+  accxt.addShapes([shape]);
+  accxt.selectShape(shape.id);
+  accxt.toView(shape.p);
+  return shape.id;
 }
